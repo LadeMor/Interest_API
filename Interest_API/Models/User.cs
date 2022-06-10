6856d4dc896463d343efdf6ba0e70a70a222687a
LadeMor <sergpyatko@gmail.com>
2022-06-10 15:01:36 +0300

    Updated User model

--- Interest_API/Models/User.cs
+++ Interest_API/Models/User.cs
@@ -1,13 +1,25 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Interest_API.Models
 {
     public class User
     {
         public int Id { get; set; }
+        
+        [Required]
+        [StringLength(20)]
         public string Username { get; set; }
+        
+        [Required]
+        [StringLength(80)]
         public string Password { get; set; }
+        
+        [Required]
+        [StringLength(50)]
         public string Email { get; set; }
+        
+        [StringLength(100)]
         public string Description { get; set; }
         public int Role { get; set; }
         public Int64 Rating { get; set; }
