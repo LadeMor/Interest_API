ae0828c56dd02ab1d85b69e79d7b09a7a890e32a
LadeMor <sergpyatko@gmail.com>
2022-06-10 15:01:53 +0300

    Added DbContext

--- Interest_API/Database/AppContext.cs
+++ Interest_API/Database/AppContext.cs
@@ -1,7 +1,16 @@
+using Interest_API.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Interest_API.Database
 {
-    public class AppContext
+    public class AppContext : DbContext
     {
+        public AppContext()
+            : base()
+        {
+        }
         
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
     }
 }
